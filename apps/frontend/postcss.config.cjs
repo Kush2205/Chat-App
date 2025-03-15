0fbb2197d3208b7d059cd16728d40b349d524650
Kush2205 <kr.kushagra2205@gmail.com>
2025-03-15 20:24:34 +0530

    Changes

---
+++ apps/frontend/postcss.config.cjs
@@ -0,0 +1,1 @@
+module.exports = require("@repo/tw-config/postcss-config");
---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Loader } from "@repo/ui/loader";
+
+
+export default function Home() {
+  return (
+   <>
+   <div>
+   <Loader/>
+   </div>
+    
+   </>
+  );
+}
